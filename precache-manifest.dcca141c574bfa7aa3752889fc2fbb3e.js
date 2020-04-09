1cf96404cdace504c4e80fc767be30c274b3ba91
HeavyCrawFish <62625909+HeavyCrawFish@users.noreply.github.com>
2020-04-09 11:44:10 +0530

    Updates

---
+++ precache-manifest.dcca141c574bfa7aa3752889fc2fbb3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c898add0dfe793d4802653770d155063",
+    "url": "/hugokill/index.html"
+  },
+  {
+    "revision": "b33ccd7a79f4a3dd24e4",
+    "url": "/hugokill/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "699e49c3796a678437e9",
+    "url": "/hugokill/static/css/main.373fccbb.chunk.css"
+  },
+  {
+    "revision": "b33ccd7a79f4a3dd24e4",
+    "url": "/hugokill/static/js/2.17c9b97b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hugokill/static/js/2.17c9b97b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "699e49c3796a678437e9",
+    "url": "/hugokill/static/js/main.5cb015ab.chunk.js"
+  },
+  {
+    "revision": "ccb1af022e3b8f0a607c",
+    "url": "/hugokill/static/js/runtime-main.54d9d73d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/hugokill/static/media/SEGA.12a3db8b.woff"
+  }
+]);
